6c442daebeb4ca02ccd1663d136cbdf27acec8c2
Alberto Luebbert M. <albertoluebbert@ideashappy.com>
2024-09-18 18:08:50 -0600

    :sparkles: Curso de Fundamentos de Programación

---
+++ fundamentos/Hola.java
@@ -0,0 +1,12 @@
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo - Grupo 54");
+
+        int valor1 = 10;
+        int valor2 = 20;
+
+        int suma = valor1 + valor2;
+
+        System.out.println("La suma es igual a " + suma);
+    }
+}
---
+++ fundamentos/hola.py
@@ -0,0 +1,11 @@
+def holaMundo():
+    print("Hola Mundo - Grupo 54")
+    
+    # Suma
+    valor1 = 10
+    valor2 = 20
+    suma = valor1 + valor2
+    
+    print(f'La suma es igual a {suma}')
+    
+holaMundo()
