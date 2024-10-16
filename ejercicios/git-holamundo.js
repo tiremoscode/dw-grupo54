c110b87f19968b535383ee8d157e01c13881ff8c
Andrea Sofia Medina Arjona <asmarjona@gmail.com>
2024-10-16 17:01:47 -0600

    Añadí mi función

--- ejercicios/git-holamundo.js
+++ ejercicios/git-holamundo.js
@@ -6,6 +6,11 @@ function helloWorld(name) {
     console.log('Hello ' + name);
 }
 
+function bonjourLeMonde(name) {
+    console.log('Bonjour le monde ' + name);
+}
+
+
 holaMundo('Alberto');
 helloWorld('Zaira');
 bonjourLeMonde('Andrea');
