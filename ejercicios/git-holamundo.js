8cdd66c2237911a8a99eba34f717585810827de1
Guadalupe Cruz <cruzsal.gpe@gmail.com>
2024-10-15 18:09:41 -0600

    Se agrega función para hola Guadalupe

--- ejercicios/git-holamundo.js
+++ ejercicios/git-holamundo.js
@@ -10,6 +10,17 @@ function ciaoMondo(nome) {
     console.log('Ciao ' + nome);
 }
 
+function niHao(_nombre){
+    console.log("ni hao " + _nombre);
+}
+
+function guadalupe(_nombre){
+    const funcionesLupita = [holaMundo, helloWorld, ciaoMondo, niHao];
+    funcionesLupita.forEach(funcion => funcion(_nombre));
+}
+
 holaMundo('Alberto');
 helloWorld('Zaira');
 ciaoMondo('Rebeca');
+guadalupe('Guadalupe');
+
