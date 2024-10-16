e47eb3a48ce73d2028a7bb18bbf49c28308bdbc6
Guadalupe Cruz <cruzsal.gpe@gmail.com>
2024-10-15 18:42:47 -0600

    Actualización Hola Guadalupe

--- ejercicios/git-holamundo.js
+++ ejercicios/git-holamundo.js
@@ -10,18 +10,22 @@ function ciaoMondo(nome) {
     console.log('Ciao ' + nome);
 }
 
-function niHao(_nombre){
-    console.log("ni hao " + _nombre);
+function holaGrupo54(alumna) {
+    console.log('Bienvenida al curso' + alumna);
 }
 
+function niHao(nombre){
+    console.log('Nihao ' + nombre);
+}
 function guadalupe(_nombre){
     const funcionesLupita = [holaMundo, helloWorld, ciaoMondo, niHao];
-    funcionesLupita.forEach(funcion => funcion(_nombre));
+    funcionesLupita.forEach(funcion => funcion('Lupita'));
 }
 
 guadalupe('Guadalupe');
 holaMundo('Alberto');
 helloWorld('Zaira');
+helloWorld('Glenis');
+helloWorld ('Sofia');
 ciaoMondo('Rebeca');
-
-
+holaGrupo54('KarlaFlores');
