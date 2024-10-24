6e5a6cb0d28efcbbfc71be7e4bb7ad8190b04970
Laura Estefania Grajeda Cardiel <lauragrajedacardiel@gmail.com>
2024-10-23 18:50:41 -0600

    :tada:Ejercicio Resuelto

---
+++ ejercicios/ejercicio_en_clase.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('¿Cómo te llamas?', (name) => {
+    rl.question('¿En que idioma quieres que te salude?, para español: 1 English: 2', (idiomaRespuesta) => {
+        switch (idiomaRespuesta){
+            case '1':
+                console.log('Hola, bienvenido al Curso de JavaScript ' + name);
+                break;
+            case '2':
+                console.log('Hello, welcome to JavaScript curse  ' + name);
+                break;
+        }
+        rl.close();
+    });
+});
+
+// ejercicio en Equipo--Clase 23/10/2024
+//Zaira
+//Laura Grajeda
+//Glenis
+//Beatriz Arellano
+//Andrea Sofia
