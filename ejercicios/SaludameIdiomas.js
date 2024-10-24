f424770ba4355774154e2246fd799900eab4ec06
Andrea Sofia Medina Arjona <asmarjona@gmail.com>
2024-10-23 21:01:43 -0600

    Le puse formatito al código

--- ejercicios/SaludameIdiomas.js
+++ ejercicios/SaludameIdiomas.js
@@ -5,41 +5,31 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-function Es(name) {
-    console.log('Hola ' + name);
-}
+function Es(name) { console.log('Hola ' + name); }
 
-function Ing(name) {
-    console.log('Hello ' + name);
-}
+function Ing(name) { console.log('Hello ' + name); }
 
-function French(name) {
-    console.log('Bonjour ' + name);
-}
+function French(name) { console.log('Bonjour ' + name); }
 
-function Italiano(name) {
-    console.log('Ciao ' + name);
-}
+function Italiano(name) { console.log('Ciao ' + name); }
 
 rl.question('¿Cómo te llamas? ', (name) => {
-rl.question('¿En qué idioma quieres que te salude? Las opciones con las que cuento son 1.Español 2.Ingles 3.Francés 4.Italiano. Escribe el número: ', (IdiomaSeleccionado) => {    
-    
-    switch (IdiomaSeleccionado){
-        case '1':
-            Es(name);
-            break;
-        case '2':
-            Ing(name);
-            break;
-        case '3':
-            French(name);
-            break;
-        case '4':
-            Italiano(name);
-            break;     
-    }
-    
+    rl.question('¿En qué idioma quieres que te salude? Las opciones con las que cuento son 1.Español 2.Ingles 3.Francés 4.Italiano. Escribe el número: ', (IdiomaSeleccionado) => {
 
-    rl.close();
-});
+        switch (IdiomaSeleccionado) {
+            case '1':
+                Es(name);
+                break;
+            case '2':
+                Ing(name);
+                break;
+            case '3':
+                French(name);
+                break;
+            case '4':
+                Italiano(name);
+                break;
+        }
+        rl.close();
+    });
 });
