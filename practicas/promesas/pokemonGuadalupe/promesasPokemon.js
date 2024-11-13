f723fd6018c6f29833c449f44d024efb97afa68c
Guadalupe Cruz <cruzsal.gpe@gmail.com>
2024-11-13 00:54:24 -0600

    Guadalupe - Actualización para Añadir las posibles evoluciones de ese pokemon, si y solo si, lo encontro enn la base de datos

--- practicas/promesas/pokemonGuadalupe/promesasPokemon.js
+++ practicas/promesas/pokemonGuadalupe/promesasPokemon.js
@@ -1,62 +1,3 @@
-const obtenerPokemon = (nombrePokemon) => {
-    return new Promise((resolve, reject) => {
-
-        // Simular una solicitud asincrona de información de un pokemón
-        const pokemonDatabase = {
-            "pikachu": {
-                nombre: "Pikachu",
-                tipo: "Eléctrico",
-                ataque: "Impact Trueno",
-                color: "Amarillo"
-            },
-            "bulbasaur": {
-                nombre: "Bulbasaur",
-                tipo: "Planta",
-                ataque: "Latigo Cepa",
-                color: "Verde"
-            },
-            "charmander": {
-                nombre: "Charmander",
-                tipo: "Fuego",
-                ataque: "Lanza llama",
-                color: "Rojo"
-            },
-            "squirtle": {
-                nombre: "Squirtle",
-                tipo: "Agua",
-                ataque: "Chorro de Agua",
-                color: "Azul"
-            },
-            "jigglypuff": {
-                nombre: "Jigglypuff",
-                tipo: "Normal/Hada",
-                ataque: "Canto",
-                color: "Rosa"
-            },
-            "meowth": {
-                nombre: "Meowth",
-                tipo: "Normal",
-                ataque: "Arañazo",
-                color: "Beige"
-            },
-            "psyduck": {
-                nombre: "Psyduck",
-                tipo: "Agua",
-                ataque: "Confusión",
-                color: "Amarillo"
-            }
-        }
-
-        // Comprobar si el pokemon existe en nuestra base de datos.
-        const pokemon = pokemonDatabase[nombrePokemon.toLowerCase()];
-        if (pokemon) {
-            resolve(pokemon);
-        } else {
-            reject('No se encontro al pokemon ' + nombrePokemon + ', capturar en futuro!');
-        }
-    })
-};
-
 const obtenerEvoluciones = (nombrePokemon) => {
     return new Promise((resolve, reject) => {
 
@@ -88,51 +29,79 @@ const obtenerEvoluciones = (nombrePokemon) => {
         // Comprobar si el pokemon existe en nuestra base de datos.
         const pokemon = pokemonDatabase[nombrePokemon.toLowerCase()];
         if (pokemon) {
-            resolve(pokemon);
+            resolve(pokemon.evoluciones);
         } else {
             reject('No se encontraron evoluciones del pokemon ' + nombrePokemon + ', registrarlas!');
         }
     })
 };
 
-const pokemon = 'jigglypuff';
+const obtenerPokemon = (nombrePokemon) => {
+    return new Promise((resolve, reject) => {
 
-obtenerPokemon(pokemon)
+        // Simular una solicitud asincrona de información de un pokemón
+        const pokemonDatabase = {
+            "pikachu": {
+                nombre: "Pikachu",
+                tipo: "Eléctrico",
+                ataque: "Impact Trueno",
+                color: "Amarillo"
+            },
+            "bulbasaur": {
+                nombre: "Bulbasaur",
+                tipo: "Planta",
+                ataque: "Latigo Cepa",
+                color: "Verde"
+            },
+            "charmander": {
+                nombre: "Charmander",
+                tipo: "Fuego",
+                ataque: "Lanza llama",
+                color: "Rojo"
+            },
+            "squirtle": {
+                nombre: "Squirtle",
+                tipo: "Agua",
+                ataque: "Chorro de Agua",
+                color: "Azul"
+            }
+        }
+
+        // Comprobar si el pokemon existe en nuestra base de datos.
+        const pokemon = pokemonDatabase[nombrePokemon.toLowerCase()];
+        if (pokemon) {
+            resolve(pokemon);
+        } else {
+            reject('No se encontro al pokemon ' + nombrePokemon + ', capturar en futuro!');
+        }
+    })
+};
+
+obtenerPokemon('pikachu')
     .then((pokemonEncontrado) => {
-        //quiero conocer la información de tipo, nombre, ataque y color
-        console.log('El nombre es ' + pokemonEncontrado.nombre + '. El tipo es ' + pokemonEncontrado.tipo +  
-            '. El ataque es ' + pokemonEncontrado.ataque + ' y su color es ' + pokemonEncontrado.color + '.'
+        return obtenerEvoluciones(pokemonEncontrado.nombre).then((evolucionesEncontradas) => {
+            pokemonEncontrado['evoluciones'] = evolucionesEncontradas
+            return pokemonEncontrado;
+        })
+    }).then((pokemonCompleto) =>{
+        console.log('El nombre es ' + pokemonCompleto.nombre + '. El tipo es ' + pokemonCompleto.tipo +  
+            '. El ataque es ' + pokemonCompleto.ataque + ' y su color es ' + pokemonCompleto.color + '. Las evoluciones son ' + pokemonCompleto.evoluciones
         );
     })
     .catch((error) => {
         console.log(error);
     });
 
-obtenerEvoluciones(pokemon)
+    obtenerPokemon('Gastly')
     .then((pokemonEncontrado) => {
-        console.log('Las evoluciones son: ' + pokemonEncontrado.evoluciones);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
-
-let pokemon2 = 'Gastly';
-
-obtenerPokemon(pokemon2)
-    .then((pokemonEncontrado) => {
-        //quiero conocer la información de tipo, nombre, ataque y color
-        console.log('El nombre es ' + pokemonEncontrado.nombre + '. El tipo es ' + pokemonEncontrado.tipo +  
-            '. El ataque es ' + pokemonEncontrado.ataque + ' y su color es ' + pokemonEncontrado.color + '.'
+        return obtenerEvoluciones(pokemonEncontrado.nombre).then((evolucionesEncontradas) => {
+            pokemonEncontrado['evoluciones'] = evolucionesEncontradas
+            return pokemonEncontrado;
+        })
+    }).then((pokemonCompleto) =>{
+        console.log('El nombre es ' + pokemonCompleto.nombre + '. El tipo es ' + pokemonCompleto.tipo +  
+            '. El ataque es ' + pokemonCompleto.ataque + ' y su color es ' + pokemonCompleto.color + '. Las evoluciones son ' + pokemonCompleto.evoluciones
         );
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
-obtenerEvoluciones(pokemon2)
-    .then((pokemonEncontrado) => {
-        console.log('Las evoluciones son: ' + pokemonEncontrado.evoluciones);
     })
     .catch((error) => {
         console.log(error);
