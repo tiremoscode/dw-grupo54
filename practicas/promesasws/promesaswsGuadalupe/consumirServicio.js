2846bdc0434afcd4fefd71161fb63361f1803395
Guadalupe Cruz <cruzsal.gpe@gmail.com>
2024-11-19 22:56:11 -0600

    Actualización API NASA, solicitarla en https://api.nasa.gov/ y agregarla en construirURL() ya que no hay backend

--- practicas/promesasws/promesaswsGuadalupe/consumirServicio.js
+++ practicas/promesasws/promesaswsGuadalupe/consumirServicio.js
@@ -1,7 +1,9 @@
+const myModal = document.getElementById('modalNasa')
+
 function limpiarHTML(_selector) {
     const elemento = document.querySelector(_selector);
     if (elemento) {
-        elemento.innerHTML = ""; // Limpia el contenido del elemento
+        elemento.innerHTML = "";
     } else {
         console.error(`Elemento con selector "${_selector}" no encontrado.`);
     }
@@ -16,17 +18,25 @@ function obtenerFechaActual(){
     return fechaFormateada;
 }
 
-function obtenerAPINasa( _fechaInicio, _fechaFin) {
-
+function construirURL( _fechaInicio, _fechaFin) {
     //Definición de parámetros.-.
-    APIKey = "9hy3ChrYfpfQQkQnfaTBtLGfuXeFrdGo844HcOVJ";
+    //Solicitar una APi Key En  
+    APIKey = null;
+    if(!APIKey){
+        console.log("Solicitar una APi Key En https://api.nasa.gov/ y agregar en código String");
+    }
 
     const fechaInicio = _fechaInicio ? _fechaInicio : obtenerFechaActual();
     const fechaFin = _fechaFin ? _fechaFin : obtenerFechaActual();
 
     //Construcción de URL, https://api.nasa.gov/ APOD Astronomy Picture of the Day .-.
-    const url = "https://api.nasa.gov/planetary/apod?api_key=" + APIKey + "&start_date=" + fechaInicio + "&end_date=" + fechaFin;
-    
+    return "https://api.nasa.gov/planetary/apod?api_key=" + APIKey + "&start_date=" + fechaInicio + "&end_date=" + fechaFin;
+}
+
+function obtenerAPINasa( _fechaInicio, _fechaFin) {
+
+    url = construirURL( _fechaInicio, _fechaFin);
+
     limpiarHTML("#crearTarjetas");
 
     fetch(url)
@@ -43,7 +53,7 @@ function obtenerAPINasa( _fechaInicio, _fechaFin) {
                     fecha = data[i].date;
                     titulo = data[i].title;
                     describe = data[i].explanation;
-                    copyright = data[i].copyright ? data[i].copyright : "Especificado en la imagen";
+                    copyright = data[i].copyright ? data[i].copyright : "";
                     urlImagen = data[i].url;
 
                     const colDiv = document.createElement('div');
@@ -56,7 +66,7 @@ function obtenerAPINasa( _fechaInicio, _fechaFin) {
 
                     cardDiv.innerHTML = imgTag;
 
-                    const describeCorto = describe.length > 50 ? describe.substring(0, 150) + "..." : describe;
+                    const describeCorto = describe.length > 150 ? describe.substring(0, 150) + "..." : describe;
 
                     // Crear el cuerpo de la tarjeta
                     const cardBody = `
@@ -64,19 +74,20 @@ function obtenerAPINasa( _fechaInicio, _fechaFin) {
                             <p class="card-text">${describeCorto}</p>
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Ver más</button>
+                                    <button type="button" data-value="${fecha}|${titulo}" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#modalNasa">Ver más</button>
                                 </div>
                                 <small class="text-body-secondary">${fecha}</small>
                             </div>
                         </div>
                     `;
+
                     cardDiv.innerHTML += cardBody;
 
                     // Insertar la tarjeta dentro del contenedor principal
                     colDiv.appendChild(cardDiv);
                     document.querySelector(elementId).appendChild(colDiv);
                 }
-                
+
             }
 
         })
@@ -85,5 +96,42 @@ function obtenerAPINasa( _fechaInicio, _fechaFin) {
         });
 }
 
+myModal.addEventListener('shown.bs.modal', (event) => {
+    const button = event.relatedTarget;
+    let obtenerValor = button.getAttribute('data-value');
+    let separarValor = obtenerValor.split('|');
+    let fechaInicial = separarValor[0];
+    let titulo = separarValor[1];
+
+    obtenerDatosModalNasa(fechaInicial, titulo);
+
+})
+
+function obtenerDatosModalNasa(_fechaInicio, _titulo){
+    //Debido a que se requiere solo 1 dato, se coloca fechaInicio en parámetro de inicio y fin.-.
+    url = construirURL( _fechaInicio, _fechaInicio);
+
+    fetch(url)
+        .then(respuesta => respuesta.json()) // convierte la respuesta en un objeto JSON.
+        .then(data => {
+            console.log('success - comunicación exitosa de una fecha');
+
+            fecha = data[0].date;
+            titulo = _titulo;
+            describe = data[0].explanation;
+            urlImagen = data[0].url;
+
+            cadena = fecha + "|" + titulo + "|" + describe + "|" + urlImagen;
+
+            document.getElementById('modalNasaFecha').textContent = fecha; 
+            document.getElementById('modalNasaTitulo').textContent = titulo; 
+            document.getElementById('modalNasaDescribe').textContent = describe; 
+            document.getElementById('modalNasaImagen').src = urlImagen; 
+
+        })
+        .catch(error => {
+            console.log('Hubo un error ', error);
+        });
+}
 //obtenerAPINasa();
 //obtenerFechaActual();
